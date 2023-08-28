56ca7d174f8206bcb8afb8afb864734c8a7fdd50
lipisandy <131548032+lipisandy@users.noreply.github.com>
2023-08-28 12:11:13 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "hashtek-terraform-state-bucket1"
+    bucket = "hashtek-terraform-state-bucket2"
     key = "main"
-    region = "ap-south-1"
-    dynamodb_table = "hashtekTerraformStatelock1"
+    region = "ap-southeast-1"
+    dynamodb_table = "hashtekTerraformstatelock"
   }
 }
