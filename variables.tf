78488b50497e5bd7abd44a7202b84b95f5098b2c
lipisandy <131548032+lipisandy@users.noreply.github.com>
2023-08-28 12:16:07 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,11 +1,11 @@
 variable "aws_region" {
        description = "The AWS region to create things in." 
-       default     = "ap-south-1" 
+       default     = "ap-southeast-1" 
 }
 
 variable "key_name" { 
     description = " SSH keys to connect to ec2 instance" 
-    default     =  "HashTek_domain" 
+    default     =  "Jenkin.pem" 
 }
 
 variable "instance_type" { 
@@ -24,7 +24,7 @@ variable "tag_name" {
 } 
 variable "ami_id" { 
     description = "AMI for Ubuntu Ec2 instance" 
-    default     = "ami-062df10d14676e201" 
+    default     = "ami-0464f90f5928bccb8" 
 }
 variable "versioning" {
     type        = bool
