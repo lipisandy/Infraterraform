0d9ef488882698f3f417b9f5cd5a05880e09c85a
lipisandy <131548032+lipisandy@users.noreply.github.com>
2023-08-29 12:39:21 +0530

    delete backend.tf

--- backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "hashtek-terraform-state-bucket2"
-    key = "main"
-    region = "ap-southeast-1"
-    dynamodb_table = "hashtekTerraformstatelock"
-  }
-}
